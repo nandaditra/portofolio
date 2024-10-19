c6b009ccd62c2f5f9b60f7173f7fc5824d73a71f
nandaditra <nandaditra56@gmail.com>
2024-10-19 15:03:39 +0700

    add font-bold

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="text-center py-4">
+        <footer className="text-center py-4 text-black font-semibold">
             copyright 2023 - created Nanda Aditya Putra
         </footer>
     )
---
+++ src/app/fxmedia/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../header'
+import Footer from '../footer'
+
+export default function FXMedia(){
+    return (
+        <div>
+               <Header />
+               <Footer />
+        </div>
+    )
+}
