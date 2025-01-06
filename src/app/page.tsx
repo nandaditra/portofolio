3617f1d6a69a4ab6a4b97f9a865ee1c4c025936a
nandaditra <nandaditra56@gmail.com>
2025-01-06 18:54:48 +0700

    add emoji

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
       </div>
 
       <div className="py-16">
-          <h1 className="text-center text-black font-bold text-3xl">Experience</h1>
+          <h1 className="text-center text-black font-bold text-4xl">My Journey Experience</h1>
           <div className="flex flex-col gap-5 py-12">
                <div className="w-full md:w-1/2 flex flex-col md:flex-row mx-auto gap-4 rounded-md border border-1 p-3">
                    <Image 
@@ -72,7 +72,8 @@ export default function Home() {
                    <div className="text-black">
                         <h3 className="font-semibold text-2xl">FXMedia Internet Pte Ltd</h3>
                         <p>Frontend Developer Intern</p>
-                        <p className="text-sm">Singapore, 2 January 2024 - 30 September 2024</p>
+                        <p className="text-sm">📬 Singapore</p>
+                        <p className="text-sm">📅 2 Jan - 30 Sep 2024</p>
                         <p className="text-sm text-justify my-3">
                            In here, I have responsiblity for assist senior engineer to research new technology, developed various project from client, fix and maintain project and learn new technology
                         </p>
@@ -93,7 +94,8 @@ export default function Home() {
                    <div className="text-black">
                         <h3 className="font-semibold text-2xl">Sagara Teknologi</h3>
                         <p>Frontend Engineer Intern</p>
-                        <p className="text-sm">Jakarta, Indonesia, 10 July 2023 - 10 October 2023</p>
+                        <p className="text-sm">📬 Jakarta, Indonesia,</p>
+                        <p className="text-sm">📅 10 Jul - 10 Oct 2023</p>
                         <p className="text-sm text-justify my-3">
                            In here, I have responsiblity to handle two project, including fix and maintain TechConsulta Project and developed jobFortech from scract using React and Tailwind
                         </p>
@@ -115,7 +117,8 @@ export default function Home() {
                    <div className="text-black">
                         <h3 className="font-semibold text-2xl">OKE Garden</h3>
                         <p>Frontend Engineer Intern</p>
-                        <p className="text-sm">Jakarta, Indonesia, 10 July 2023 - 10 October 2023</p>
+                        <p className="text-sm">📬 Jakarta, Indonesia</p>
+                        <p className="text-sm">📅 10 Jul - 10 Oct 2023</p>
                         <p className="text-sm text-justify my-3">
                            In here, I collaborate with cross functional team to developed and integrated RAB Calculator feature with Product Manager, UI/UX Designer, and Backend Developer</p>
 
