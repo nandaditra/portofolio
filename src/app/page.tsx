b69aaa9bcd1a8c0ba1e76436697db5d6b1642609
nandaditra <nandaditra56@gmail.com>
2025-01-07 16:53:35 +0700

    remove logo company

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,16 +60,7 @@ export default function Home() {
           <h1 className="text-center text-black font-bold text-4xl">#My Careey Journey</h1>
           <p className="text-center text-black text-xl">Experience</p>
           <div className="flex flex-col gap-5 py-12">
-               <div className="w-full md:w-1/2 flex flex-col md:flex-row mx-auto gap-4 rounded-md border border-1 p-3">
-                   <Image 
-                     className="relative mx-auto"
-                     src="/assets/img/FXMedia/fxmedia_fxmweb_logo.jpg"
-                     alt="FXMedia Internet Pte Ltd Logo"
-                     width={200}
-                     height={200}
-                     priority
-                   />
-
+               <div className="w-full md:w-1/2 flex flex-col md:flex-row mx-auto gap-4">
                    <div className="text-black">
                         <h3 className="font-semibold text-2xl">FXMedia Internet Pte Ltd</h3>
                         <p>Frontend Developer Intern</p>
@@ -79,19 +70,10 @@ export default function Home() {
                            In here, I have responsiblity for assist senior engineer to research new technology, developed various project from client, fix and maintain project and learn new technology
                         </p>
 
-                        <button className="text-sm float-right font-semibold"><Link href="/fxmedia">View Project</Link></button>
+                        <button className="text-sm float-right font-semibold bg-black text-white p-2 rounded-md"><Link href="/fxmedia">View Project</Link></button>
                    </div>
                </div>
-               <div  className="w-full md:w-1/2 flex flex-col md:flex-row mx-auto gap-4 rounded-md border border-1 p-3">
-                   <Image 
-                     className="relative mx-auto"
-                     src="/assets/img/SagaraTeknologi/sagara.png"
-                     alt="Sagara Teknologi"
-                     width={200}
-                     height={200}
-                     priority
-                   />
-
+               <div  className="w-full md:w-1/2 flex flex-col md:flex-row mx-auto gap-4">
                    <div className="text-black">
                         <h3 className="font-semibold text-2xl">Sagara Teknologi</h3>
                         <p>Frontend Engineer Intern</p>
@@ -101,29 +83,20 @@ export default function Home() {
                            In here, I have responsiblity to handle two project, including fix and maintain TechConsulta Project and developed jobFortech from scract using React and Tailwind
                         </p>
 
-                        <button className="text-sm float-right font-semibold"><Link href="/sagara">View Project</Link></button>
+                        <button className="text-sm float-right font-semibold bg-black text-white p-2 rounded-md"><Link href="/sagara">View Project</Link></button>
                    </div>
                </div>
 
-               <div  className="w-full md:w-1/2 flex flex-col mx-auto md:flex-row gap-4 rounded-md border border-1 p-3">
-                   <Image 
-                     className="relative mx-auto"
-                     src="/assets/img/OKEGarden/okegarden_logo.jpg"
-                     alt="Sagara Teknologi"
-                     width={200}
-                     height={200}
-                     priority
-                   />
-
+               <div  className="w-full md:w-1/2 flex flex-col mx-auto md:flex-row gap-4">
                    <div className="text-black">
                         <h3 className="font-semibold text-2xl">OKE Garden</h3>
                         <p>Frontend Engineer Intern</p>
                         <p className="text-sm">📬 Jakarta, Indonesia</p>
-                        <p className="text-sm">📅 10 Jul - 10 Oct 2023</p>
+                        <p className="text-sm">📅 10 Jan - 10 Apr 2023</p>
                         <p className="text-sm text-justify my-3">
                            In here, I collaborate with cross functional team to developed and integrated RAB Calculator feature with Product Manager, UI/UX Designer, and Backend Developer</p>
 
-                        <button className="text-sm float-right font-semibold"><Link href="/okegarden">View Project</Link></button>
+                        <button className="text-sm float-right font-semibold bg-black text-white p-2 rounded-md"><Link href="/okegarden">View Project</Link></button>
                    </div>
                </div>
           </div>
@@ -146,8 +119,6 @@ export default function Home() {
                      <h3 className="text-3xl font-bold">Bestimall</h3>
                      <p className="text-md mt-2">E-commerce website which provides various product from fakestore api and payment feature from stripe. This project build with Angular and Tailwind. For Payment using third party library Stripe and Firebase for authentication</p>
                      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 my-4">
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Html</button>
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Css</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Typescript</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Angular</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Tailwind</button>
@@ -173,8 +144,6 @@ export default function Home() {
                      <h3 className="text-3xl font-bold">Nontonmovie</h3>
                      <p className="text-md mt-2">Movie website which provides various genre theme in movie. This website build using Angular and tailwind with API from moviedb api. For authentication and database using firebase for real time database</p>
                      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 my-4">
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Html</button>
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Css</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Typescript</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Angular</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Tailwind</button>
@@ -200,8 +169,6 @@ export default function Home() {
                      <h3 className="text-3xl font-bold">Freephoto</h3>
                      <p className="text-md mt-2">Movie website which provides various genre theme in movie. This website build using Angular and tailwind with API from moviedb api. For authentication and database using firebase for real time database</p>
                      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 my-4">
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Html</button>
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Css</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Javascript</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">React</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Tailwind</button>
@@ -211,6 +178,32 @@ export default function Home() {
                      <div className="flex gap-4 float-right  pb-3">
                         <a className="font-bold" href="https://github.com/nandaditra/freePhoto-frontend">Github</a> 
                         <a className="font-bold" href="https://free-photo-frontend.vercel.app/">Project</a>
+                     </div>
+                  </div>
+               </div>
+               <div className="rounded-md drop-shadow border border-1">
+                  <Image 
+                     className="rounded-md"
+                     src="/assets/img/project/Ardes/ardes-fe.png"
+                     alt="Ardes"
+                     width={1000}
+                     height={400}
+                     priority
+                  />
+                  <div className="text-black p-4">
+                     <h3 className="text-3xl font-bold">Ardes</h3>
+                     <p className="text-md mt-2">Developed website that provides services for hiring architects and designers to build houses, apartments or residences
+                     using React, Bootstrap Typescript, MySQL, PHP and AWS</p>
+                     <div className="grid grid-cols-3 md:grid-cols-5 gap-4 my-4">
+                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">React</button>
+                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Bootstrap</button>
+                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Typescript</button>
+                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">MySQL</button>
+                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">PHP</button>
+                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">AWS</button>
+                     </div>
+                     <div className="flex gap-4 float-right pb-3">
+                        <a className="font-bold" href="https://github.com/nandaditra/ardes">Github</a> 
                      </div>
                   </div>
                </div>
@@ -227,8 +220,6 @@ export default function Home() {
                      <h3 className="text-3xl font-bold">Travel Tour</h3>
                      <p className="text-md mt-2">Travel Tour is a website which avaliable the holiday service trust and has been widely used by tourists so you can holiday funny and enjoy no problem. Therefore, you can choose travel package based on your interested and you will excited.</p>
                      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 my-4">
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Html</button>
-                        <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Css</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Javascript</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">React</button>
                         <button className="outline outline-offset-1 outline-black px-3 rounded-md text-black hover:bg-black hover:text-white">Bootstrap</button>
